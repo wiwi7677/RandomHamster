ff1709fa995efeb13cb750073c5fea53ba8dc9f4
Wiwi <wiwi880331@gmail.com>
2021-06-22 18:37:01 +0800

    Initial Commit

---
+++ RandomHamster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomHamster
+//
+//  Created by wiwi on 2021/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
